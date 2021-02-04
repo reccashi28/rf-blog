c0fe31e83d5425cd7a64646ab0c0f3955858e464
Rebecca Meyer <faustorebeccabsit@gmail.com>
2021-02-04 21:41:13 +0100

    exercising react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Button from './Button';
 
 function App() {
+
+const [count, setCount] = useState(0)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>You clicked the button {count} times</p>
+      <Button setCount={setCount } count={count} />
     </div>
   );
 }
