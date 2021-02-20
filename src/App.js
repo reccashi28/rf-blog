ce3b976025b555c014835ea91cc694a78c37c57e
Rebecca Meyer <faustorebeccabsit@gmail.com>
2021-02-20 18:30:08 +0100

    adding useeffect function, fetching data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Navigator from './components/Navigator/Navigator';
 import Content from './components/Content/Content';
@@ -8,11 +8,19 @@ import Content from './components/Content/Content';
 function App() {
 
 // const [count, setCount] = useState(0)
-const [blogs, setBlogs] = useState([
-  {title: "Paradise on Earth", content: "1. Arrays of objects don't stay the same all the time.", author: "peter", id: 1},
-  {title: "Eternal Life", content: "2. Arrays of objects don't stay the same all the time.", author: "john", id: 2},
-  {title: "Health", content: "3. Arrays of objects don't stay the same all the time.", author: "john", id: 3},
-]);
+// const [blogs, setBlogs] = useState([
+//   {title: "Paradise on Earth", content: "1. Arrays of objects don't stay the same all the time.", author: "peter", id: 1},
+//   {title: "Eternal Life", content: "2. Arrays of objects don't stay the same all the time.", author: "john", id: 2},
+//   {title: "Health", content: "3. Arrays of objects don't stay the same all the time.", author: "john", id: 3},
+// ]);
+
+const [blogs, setBlogs] = useState([]);
+
+useEffect( () => {
+  fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(response => response.json())
+  .then(data => setBlogs(data))
+}, []);
 
 const handleDelete = (id) => {
   const newBlogsList = blogs.filter( blog => blog.id !== id);
--- src/components/BlogsList/BlogsList.js
+++ src/components/BlogsList/BlogsList.js
@@ -6,8 +6,8 @@ const BlogsList = ({blogs, handleDelete}) => {
            { blogs.map( (blog) => (
                <div className="bloglist-content" key={blog.id}>
                    <div>
-                        <h5 className="blogpost-title">{blog.title}</h5>
-                        <p>{blog.content}</p>
+           <h5 className="blogpost-title">{blog.id}. {blog.title}</h5>
+                        <p>{blog.body}</p>
                    </div>
                     
                     <button className="delete-post" onClick= { () => handleDelete(blog.id)}>Delete</button>
