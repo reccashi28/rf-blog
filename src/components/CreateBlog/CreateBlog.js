447734820c4102020218d778edeccd5ab16d7b85
Rebecca Meyer <faustorebeccabsit@gmail.com>
2021-03-11 22:44:42 +0100

    adding form

--- src/components/CreateBlog/CreateBlog.js
+++ src/components/CreateBlog/CreateBlog.js
@@ -2,6 +2,13 @@ const CreateBlog = () => {
     return(
         <div className="create-blog home">
             <h1>Create Blog</h1>
+            <div>
+                <form action="submit">
+                    <label htmlFor="blog-title"></label>
+                    <input type="text"/>
+                </form>
+            </div>
+
         </div>
     );
 }
